f91290f0a0e9b0df6ceabc9866a0ebabfe0827f1
Ibrahim Ismayilov <i.ismayilov@EDU.epsb.ca>
2023-05-19 10:30:19 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
+# Data Management Project by Ibrahim Ismayilov
 
-numbers = []
-for num in range(10, 1001, 10):
-    numbers.append(num)
+# Base Project Single User
 
-print(numbers)
+# Array to Store Data
+books = [[{"id": 1, "title": "fdasfd"}, {"id": 2, "title": "fdafdasfsda"}]]
+shopping_cart = []
+
+def new_book(title, ISBN, genre, author):
+        return {
+            "title": title,
+            "ISBN": ISBN,
+            "genre": genre,
+            "author": author
+        }
+
+
+
+
+# Menu Loop
+while loop:
+        # Print Main Menu
+        print("1. Spell Check a Word (Linear Search)")
+        print("2. Spell Check a Word (Binary Search)")
+        print("3. Spell Check Alice in Wonderland (Linear Search)")
+        print("3. Spell Check Alice in Wonderland (Binary Search)")
+        print("4. Spell Check Alice in Wonderland (Binary Search)")
+        print("5. Exit")
+
+[{"id": 1, "title": "fdasfd"}, {"id": 2, "title": "fdafdasfsda"}]
